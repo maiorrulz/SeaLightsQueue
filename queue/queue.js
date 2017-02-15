3172288a06ce4dc50883078c916a43e0a35356a6
Alex <maiorrulz@gmail.com>
2017-02-15 03:18:20 +0200

    Comments

--- queue/queue.js
+++ queue/queue.js
@@ -5,11 +5,15 @@ var Queue = function () {
 };
 
 (function () {
+
     var queue = [];
 
+    /**
+     * Enqueue element to Queue. Element goes to the end of queue
+     *
+     * @param element - element to enqueue. Cannot be undefined or nulll
+     */
     Queue.prototype.enqueue = function (element) {
-
-        // QueueManager.responses[this.name].cb();
 
         console.log("Pushing Element : " + element);
         if (element !== undefined && element !== null)
@@ -17,7 +21,11 @@ var Queue = function () {
     };
 
 
-
+    /**
+     * Dequeue from the head of queue.
+     * The most old element will be returned
+     * @returns {*} return dequeued element or throw exception if queue is  empty.
+     */
     Queue.prototype.dequeue = function () {
         if (queue.length > 0) {
             var i = queue.shift();
@@ -29,19 +37,30 @@ var Queue = function () {
         }
     };
 
-
+    /**
+     * Get current status of elements (snapshot) in queue.
+     * @returns {Array} queue array
+     */
     Queue.prototype.getSnapshot = function () {
         console.log("Snapshot : " + this.queue);
         return queue;
     };
 
+    /**
+     * Get queue size
+     * @returns {Number} - number of elements in queue
+     */
     Queue.prototype.getQueueSize = function () {
 
         return queue.length;
     };
+
+    /**
+     * Clear Queue
+     */
     Queue.prototype.clearQueue = function () {
 
-        return queue.length =0;
+        queue.length =0;
     };
 })();
 
